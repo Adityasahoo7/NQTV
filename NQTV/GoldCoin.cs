492190365c776785587e6b21f54d2d8bd9d860e6
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-03-19 00:05:19 +0530

    Initial Push

---
+++ NQTV/GoldCoin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NQTV
+{
+    public class GoldCoin
+    {
+
+        public static void Main(string[] args) {
+
+            HorseCount.print();
+
+            //Console.WriteLine("Hi Aditya !");
+        
+        }
+    }
+}
---
+++ NQTV/HorseCount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NQTV
+{
+    public class HorseCount
+    {
+        public static void print()
+        {
+            Console.WriteLine("It is Horse Count");
+        }
+    }
+}
